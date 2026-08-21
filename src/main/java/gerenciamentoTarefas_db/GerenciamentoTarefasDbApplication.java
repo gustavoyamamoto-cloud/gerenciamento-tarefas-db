5853a0be05a8a99a6ead567a3946d8519e4b0fee
gustavoyamamoto-cloud <gustavoyamamoto@alunos.utfpr.edu.br>
2026-08-21 13:39:54 -0300

    Inicio do projeto

---
+++ src/main/java/gerenciamentoTarefas_db/GerenciamentoTarefasDbApplication.java
@@ -0,0 +1,13 @@
+package gerenciamentoTarefas_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoTarefasDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoTarefasDbApplication.class, args);
+	}
+
+}
