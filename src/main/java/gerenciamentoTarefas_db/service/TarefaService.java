88f32140c4afd82ff578fbe6e6ee072322bfc4e1
gustavoyamamoto-cloud <gustavoyamamoto@alunos.utfpr.edu.br>
2026-08-28 09:21:38 -0300

    Endepoint Listar

--- src/main/java/gerenciamentoTarefas_db/service/TarefaService.java
+++ src/main/java/gerenciamentoTarefas_db/service/TarefaService.java
@@ -1,7 +1,11 @@
 package gerenciamentoTarefas_db.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import gerenciamentoTarefas_db.dto.TarefasResponse;
+import gerenciamentoTarefas_db.model.Tarefa;
 import gerenciamentoTarefas_db.repository.TarefaRepository;
 
 @Service
@@ -13,5 +17,20 @@ public class TarefaService {
         this.repository = repository;
     }
 
+
+    //Listar
+    public List<TarefasResponse> listar(){
+        
+        return repository.findAll()
+                            .stream()
+                            .map(t -> new TarefasResponse(
+                                t.getId(),
+                                t.getTitulo(),
+                                t.getDescricao(),
+                                t.getPrioridade(),
+                                t.getStatus(),
+                                t.getDataCriacao()))
+                            .toList();
+    }
     
 }
