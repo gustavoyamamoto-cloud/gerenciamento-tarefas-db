e42a5ef101aea077da02c7a0808f59cfba8a0488
gustavoyamamoto-cloud <gustavoyamamoto@alunos.utfpr.edu.br>
2026-08-28 09:11:04 -0300

    Cria repository

---
+++ src/main/java/gerenciamentoTarefas_db/repository/TarefaRepository.java
@@ -0,0 +1,11 @@
+package gerenciamentoTarefas_db.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import gerenciamentoTarefas_db.model.Tarefa;
+
+@Repository
+public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+    
+}
